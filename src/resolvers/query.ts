9851c2ff97d937d51091df04de896b926954247e
rafa <rafaelsistem87@gmail.com>
2021-02-14 00:38:35 -0700

    Resolvers - Queries - Información del estudiante seleccionado
     Resolvers - Queries - Información del estudiante seleccionado

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -5,6 +5,19 @@ const query : IResolvers = {
       Query: {
             estudiantes(): any {
                   return database.estudiantes
+            },
+            estudiante(__: void, {id}): any {
+                  const resultado = database.estudiantes.filter(estudiante => estudiante.id === id) [0]
+                  if(resultado === undefined){
+                        return{
+                              id: '-1',
+                              name: `No se encontro al estudiante con el numero: ${id}`,
+                        email:'',
+                  courses: []                        }
+                  }
+
+
+                  return resultado
             }
       }
 }
